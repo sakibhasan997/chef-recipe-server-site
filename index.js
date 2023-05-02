4c71a265b282bb5adfcc2b1e699ee307054f78bb
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-05-02 11:18:19 +0600

    check is get app

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const recipes = require('./data/chef-details.json')
 
 app.use(cors());
 app.get('/', (req, res)=>{
-    res.send('bro is running clear the road fast jow ki')
+    res.send('bro is running clear the road fast jow ki ase jibone')
     console.log('broo');
-})
+});
 
 app.get('/recipes', (req, res)=>{
     res.send(recipes);
-})
+});
 
 
 app.listen(port, ()=>{
